51638e1260be280e2bced4905df312c54b3c5c55
sargeliu <53758352+Sargeliu@users.noreply.github.com>
2024-05-19 20:46:01 +0500

    Menu controller

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:web/layout.dart';
 
 void main() {
+  Get.put(MenuController());
   runApp(MyApp());
 }
 
---
+++ lib/routing/routes.dart
@@ -0,0 +1,15 @@
+// ignore_for_file: constant_identifier_names
+
+import 'dart:html';
+
+const OverViewPageRoute = 'Overview';
+const DriversPageRoute = 'Drivers';
+const ClientsPageRoute = 'Clients';
+const AuthenticationPageRoute = 'Authentication';
+
+List sideMenuItems = [
+  OverViewPageRoute,
+  DriversPageRoute,
+  ClientsPageRoute,
+  AuthenticationPageRoute
+];
